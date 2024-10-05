692a7fd7df3e3fda73368e7b19a151e1610d6f8a
Abayomi Aishida <yomicorperate@gmail.com>
2024-10-05 10:32:54 +0100

    Fix: base url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,8 @@
 const apiUrl = import.meta.env.VITE_BASE_URL || "/";
 
-export const BASE_URL = apiUrl; // If using proxy
+export const BASE_URL = `${apiUrl}`; // If using proxy
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/auth";
 export const ORDERS_URL = "/api/orders";
 export const PAYPAL_URL = "/api/config/paypal";
+console.log(BASE_URL);
